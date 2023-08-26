9e1e9b97edcdf6d8d08505b04fa133efbe947518
Alex Rojas <a.rojas8907@gmail.com>
2023-08-26 15:33:27 +0000

    fixed build-env.sh

--- build-env.sh
+++ build-env.sh
@@ -5,6 +5,5 @@ basedir=$( cd "$(dirname "$0")" ; pwd -P)
 conda create --name osgeo-env python=3.11
 conda env list
 # here we want the algorithm job build to fail
-kcbnzx
 echo didnt break?
 # conda env update --name base -f ${basedir}/environment.yml
