d5fddb2392e44bb25d069f2d46a9bd74afaffdbb
Alex Rojas <a.rojas8907@gmail.com>
2023-08-26 15:20:51 +0000

    added command conda env list to build-env.sh

--- build-env.sh
+++ build-env.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-source activate base
+# source activate base
 basedir=$( cd "$(dirname "$0")" ; pwd -P)
-conda env update --name base -f ${basedir}/environment.yml
+conda create --name osgeo-env python=3.11
+conda env list
+# here we want the algorithm job build to fail
+kcbnzx
+echo didnt break?
+# conda env update --name base -f ${basedir}/environment.yml
