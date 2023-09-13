01dc2370c2beeaa450db4f22f0c8db297240f848
Alex Rojas <a.rojas8907@gmail.com>
2023-09-13 04:23:05 +0000

    updated environment.yml and build-env.sh

--- build-env.sh
+++ build-env.sh
@@ -5,3 +5,9 @@ basedir=$( cd "$(dirname "$0")" ; pwd -P)
 conda env list
 echo installing environment...
 conda env create -f ${basedir}/environment.yml
+
+# Install the maap.py environment
+conda activate osgeo-env
+git clone --single-branch --branch v3.0.1 https://github.com/MAAP-Project/maap-py.git ${basedir}
+cd ${basedir}/maap-py
+pip install -e .
