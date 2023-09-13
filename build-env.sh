6ade147bf7cdc0b3b6269198bbee002f27939185
Alex Rojas <a.rojas8907@gmail.com>
2023-09-13 23:51:33 +0000

    updated build-env.sh

--- build-env.sh
+++ build-env.sh
@@ -3,15 +3,17 @@
 # source activate base
 basedir=$( cd "$(dirname "$0")" ; pwd -P)
 echo installing environment...
-# mamba env create -f ${basedir}/environment.yml
-conda create --name osgeo-env --clone base
-source activate osgeo-env
-mamba install -c conda-forge scipy -y
-mamba install -c conda-forge h5py -y
-mamba install -c conda-forge geopandas -y
+mamba env create -f ${basedir}/environment.yml
+# conda create --name osgeo-env --clone base
+# source activate osgeo-env
+# mamba install -c conda-forge scipy -y
+# mamba install -c conda-forge h5py -y
+# mamba install -c conda-forge geopandas -y
 
 # Install the maap.py environment
-# source activate osgeo-env
-# git clone --single-branch --branch v3.0.1 https://github.com/MAAP-Project/maap-py.git ${basedir}
-# cd ${basedir}/maap-py
-# pip install -e .
+echo trying to install maap-py...
+source activate osgeo-env
+git clone --single-branch --branch v3.0.1 https://github.com/MAAP-Project/maap-py.git
+cd maap-py
+pip install -e .
+echo installed maap-py package!
