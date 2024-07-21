8cd05975f0c012a9027990b7cfd0a6f2527b5918
Ian Grant <ian.conway.grant@gmail.com>
2024-07-21 14:31:30 -0400

    cleaned up run script

--- run.sh
+++ run.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-# Gets directory of .sh file
+# Get directory of run script
 basedir=$( cd "$(dirname "$0")" ; pwd -P)
 
-# Activate environment that was created in the builed-env.sh file
-source activate nmbim-env
+# Activate environment that was created in the build-env.sh file
+conda activate nmbim-env
 
 # Create output dir
-# OUTPUTDIR="${PWD}/output"
 mkdir -p output
-# INPUT_FILES
+
+# URLs to L1B and L2A input files
 L1B_URL=$1
-L2A_URL=$2 # e.g. GEDI01_B or GEDI02_A
-# YR=$3
-# python ${basedir}/main.py ${L1B_URL} ${L2A_URL} ${OUTPUTDIR}
+L2A_URL=$2
+
 python ${basedir}/main.py ${L1B_URL} ${L2A_URL} output
