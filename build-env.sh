21b494e8c65bc2443f35257fc9bb281a867ebc29
Ian Grant <ian.conway.grant@gmail.com>
2024-07-21 15:01:01 -0400

    cleaned up build script

--- build-env.sh
+++ build-env.sh
@@ -2,6 +2,5 @@
 
 # source activate base
 basedir=$( cd "$(dirname "$0")" ; pwd -P)
-echo installing environment...
+echo "Creating conda environment from ${basedir}/environment.yml"
 conda env create --name nmbim-env -f ${basedir}/environment.yml
-
