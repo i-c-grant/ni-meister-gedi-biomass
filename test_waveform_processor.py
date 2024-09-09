079b467968e7dd2a45e9349847971ca2a37c3b38
Ian Grant <ian.conway.grant@gmail.com>
2024-09-09 11:36:54 -0400

    updated test to write to single GeoPackage

--- test_waveform_processor.py
+++ test_waveform_processor.py
@@ -1,10 +1,17 @@
+import argparse
+from datetime import datetime
+import cProfile
+import os
+from pathlib import Path
+
+import h5py
+
+import nmbim.algorithms as algorithms
+import nmbim.processing_pipelines as processing_pipelines
+from nmbim.CachedBeam import CachedBeam
 from nmbim.Waveform import Waveform
 from nmbim.WaveformProcessor import WaveformProcessor
-import nmbim.algorithms as algorithms
-from nmbim.CachedBeam import CachedBeam
-import h5py
-import argparse
-import cProfile
+from nmbim.WaveformWriter import WaveformWriter
 
 
 def main():
@@ -31,94 +38,65 @@ def main():
     else:
         num_waveforms = None
 
-    l1b = h5py.File(l1b_path, "r")
-    l2a = h5py.File(l2a_path, "r")
+    # Create output directory with timestamp
+    output_dir = f"tests/output_{datetime.now().strftime('%Y%m%d_%H%M%S')}"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
     # Define processor parameters
-    processor_params = {
-        "noise": {"alg_fun" : algorithms.remove_noise,
-                  "input_map" : {"wf": "raw/wf",
-                               "mean_noise": "raw/mean_noise"},
-                  "output_path" : "processed/wf_noise_removed",
-                  "params" : {}},
-        "normalize": {"alg_fun" : algorithms.normalize_waveform,
-                      "input_map" : {"wf": "processed/wf_noise_removed"},
-                      "output_path" : "processed/wf_noise_normalized",
-                      "params" : {}},
-        "smooth": {"alg_fun" : algorithms.smooth_waveform,
-                   "input_map" : {"wf": "processed/wf_noise_normalized"},
-                   "output_path" : "processed/wf_noise_normalized_smoothed",
-                   "params" : {"sd": 5}},
-        "per_height": {"alg_fun" : algorithms.calc_wf_per_height,
-                       "input_map" : {"wf": "processed/wf_noise_normalized_smoothed"},
-                       "output_path" : "processed/wf_per_height",
-                       "params" : {"dz": .1}},
-        "segment": {"alg_fun" : algorithms.separate_veg_ground,
-                    "input_map" : {"wf": "processed/wf_per_height",
-                                 "ht": "processed/ht",
-                                 "rh": "raw/rh"},
-                    "output_path" : "processed/wf_segmented",
-                    "params" : {"veg_floor": 5}},
-        "height": {"alg_fun" : algorithms.calc_height,
-                   "input_map" : {"wf": "raw/wf",
-                                "elev_top": "raw/elev/top",
-                                "elev_bottom": "raw/elev/bottom",
-                                "elev_ground": "raw/elev/ground"},
-                   "output_path" : "processed/ht",
-                   "params" : {}}
-    }
+    processor_params = processing_pipelines.pipeline_biomass_index_simple
 
-    # Get beam names, excluding metadata group
-    beams = list(l1b.keys())[:-1]
+    with h5py.File(l1b_path, "r") as l1b, h5py.File(l2a_path, "r") as l2a:
+        # Get beam names, excluding metadata group
+        beams = [key for key in l1b.keys() if key != 'METADATA']
 
-    for beam in beams:
+        for beam in beams:
 
-        # Load beam data into memory if caching is enabled
-        # Otherwise, access data directly from file
-        if args.cache:
-            l1b_beam = CachedBeam(l1b, beam)
-            l2a_beam = CachedBeam(l2a, beam)
-        else:
-            l1b_beam = l1b[beam]
-            l2a_beam = l2a[beam]
+            # Load beam data into memory if caching is enabled
+            # Otherwise, access data directly from file
+            if args.cache:
+                l1b_beam = CachedBeam(l1b, beam)
+                l2a_beam = CachedBeam(l2a, beam)
+            else:
+                l1b_beam = l1b[beam]
+                l2a_beam = l2a[beam]
 
-        if num_waveforms is not None:
-            shot_numbers = l1b[beam]["shot_number"][:num_waveforms]
-        else:
-            shot_numbers = l1b[beam]["shot_number"][:]
+            # Limit number of waveforms to process if limit was specified
+            if num_waveforms is not None:
+                shot_numbers = l1b[beam]["shot_number"][:num_waveforms]
 
-        print(f"Processing {len(shot_numbers)} waveforms for beam {beam}")
-        
-        # Define waveform arguments that don't change between waveforms
-        waveform_args = {"l1b_beam": l1b_beam, "l2a_beam": l2a_beam}
+            else:
+                shot_numbers = l1b[beam]["shot_number"][:]
 
-        # Create set to store waveforms, ensuring no duplicates
-        waveforms = set()
+            print(f"Processing {len(shot_numbers)} waveforms for beam {beam}")
 
-        # Create waveforms
-        for shot_number in shot_numbers:
-            waveform_args["shot_number"] = shot_number
-            waveforms.add(Waveform(**waveform_args))
+            # Define within-beam waveform arguments that don't change between waveforms
+            waveform_args = {"l1b_beam": l1b_beam, "l2a_beam": l2a_beam}
 
-        # Create processors
-        proc_list = ["height",
-                     "noise",
-                     "normalize",
-                     "smooth"]
+            # Create set of waveforms for this beam, ensuring no duplicates
+            waveforms = set()
+            for shot_number in shot_numbers:
+                waveform_args["shot_number"] = shot_number
+                waveforms.add(Waveform(**waveform_args))
 
-        pipeline = []
+            # Create pipeline of processors for this beam
+            pipeline = []
+            for proc_name in processor_params:
+                p = WaveformProcessor(**processor_params[proc_name], waveforms = waveforms)
+                pipeline.append(p)
 
-        for proc_name in proc_list:
-            p = WaveformProcessor(**processor_params[proc_name], waveforms = waveforms)
-            pipeline.append(p)
-
-        # Process waveforms, applying each processor in order
-        for p in pipeline:
-            p.process()
-
-    l1b.close()
-    l2a.close()
-
+            # Process waveforms, applying each processor in order
+            for p in pipeline:
+                p.process()
+                
+            # Write processed data to GeoPackage
+            waveform_writer = WaveformWriter(path=f"{output_dir}/test.gpkg",
+                                             cols={"biwf": "results/biomass_index",
+                                                   "lat": "metadata/coords/lat",
+                                                   "lon": "metadata/coords/lon"},
+                                             append=True,
+                                             waveforms=waveforms)
+            waveform_writer.write()
 
 if __name__ == "__main__":
     main()
