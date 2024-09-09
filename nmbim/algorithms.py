3f6038bf797fd10bbfcd0b8be6246837302eb264
Ian Grant <ian.conway.grant@gmail.com>
2024-09-09 11:36:30 -0400

    debugged ground separation algorithm

--- nmbim/algorithms.py
+++ nmbim/algorithms.py
@@ -184,15 +184,14 @@ def separate_veg_ground(
     rh : ArrayLike
         Relative height values for waveform.
     veg_floor : IntOrFloat, optional
-        Minimum height above ground of bottom-most (i.e. last) veg. return,
-        by default 5 m.
+        Minimum height above ground of bottom-most (i.e. last) veg. return.
 
     Returns
     -------
     Dict
-        Dictionary with keys "ground_top", "ground_bottom", "veg_top", and "veg_bottom" and values giving the correspoding indices within the waveform. 
-"""
-    
+        Dictionary with keys "ground_top", "ground_bottom", "veg_top", and "veg_bottom" and values giving the corresponding indices within the waveform.
+    """
+
     # Get index of first vegetation return (top of canopy)
     veg_first_idx = np.argmax(ht <= rh[100])
 
@@ -201,35 +200,32 @@ def separate_veg_ground(
     ground_idx = np.where(np.absolute(ht) == min_height)[0][0]
 
     # Calculate waveform's noise level from part of waveform above vegetation
-    wf_above_veg = wf[veg_first_idx:]
+    wf_above_veg = wf[0:veg_first_idx]
     noise = np.std(wf_above_veg) * 2
 
     # Find indices below first ground return that are below noise level
     # (indexes are relative to the first ground return)
-    below_ground_noise_idxs = np.where(wf[:ground_idx] < noise)[0]
+    below_ground_noise_idxs = np.where(wf[ground_idx:] < noise)[0]
 
-    # Check if any below-noise returns were found below first ground return
-    if (len(below_ground_noise_idxs) == 0):
-        # If not, redefine noise level using waveform until ground return
+    # Check whether below-noise returns were found below first ground return
+    if len(below_ground_noise_idxs) == 0:
+        # If not, redefine noise level using full above-ground waveform
         wf_above_ground = wf[:ground_idx - 1]
         noise = np.std(wf_above_ground) * 2
         # Find below-noise indices with new noise level
-        below_ground_noise_idxs = np.where(wf[:ground_idx] < noise)[0]
+        below_ground_noise_idxs = np.where(wf[ground_idx:] < noise)[0]
         # If still no below-noise returns, raise exception
-        if (len(below_ground_noise_idxs) == 0):
-            raise ValueError(f"No returns below noise level {noise} found below ground return")
+        if len(below_ground_noise_idxs) == 0:
+            raise ValueError(
+                f"No returns below noise level {noise} found below ground return"
+            )
 
-    min_ground_noise_idx = np.min(below_ground_noise_idxs)
-
-    if ground_idx + min_ground_noise_idx < len(wf):
-        last_ground_idx = ground_idx + min_ground_noise_idx
-    else:
-        last_ground_idx = len(wf) - 1
-
-    # Calculate first ground index, assuming symmetric ground return
-    last_ground_height = ht[last_ground_idx]
-    first_ground_height = -last_ground_height
-    first_ground_idx = np.argmax(ht <= first_ground_height)
+    # Ground return index +/- ground offset gives the indices
+    # in the ground return region
+    ground_offset = np.min(below_ground_noise_idxs)
+    breakpoint()
+    last_ground_idx = np.min(ground_idx + ground_offset, len(wf) - 1)
+    first_ground_idx = np.max(ground_idx - ground_offset, 0)
 
     # Calculate last vegetation return index
     last_veg_height = np.min([veg_floor, -ht[last_ground_idx]])
@@ -244,10 +240,13 @@ def separate_veg_ground(
 
     return ans
 
-def calc_gap_prob(wf_per_height: np.ndarray,
-                  veg_first_idx: int,
-                  veg_last_idx: int,
-                  ground_last_idx: int) -> dict:
+
+def calc_gap_prob(
+    wf_per_height: np.ndarray,
+    veg_first_idx: int,
+    veg_last_idx: int,
+    ground_last_idx: int,
+) -> dict:
     """
     Calculate gap probability, vegetation cover, and related metrics for a single waveform.
 
@@ -269,23 +268,25 @@ def calc_gap_prob(wf_per_height: np.ndarray,
     """
 
     # Calculate vegetation and ground return sums
-    veg_sum = np.nansum(wf_per_height[veg_first_idx: veg_last_idx])
+    veg_sum = np.nansum(wf_per_height[veg_first_idx:veg_last_idx])
     # TODO: could instead go from first to last ground return?
-    ground_sum = np.nansum(wf_per_height[veg_last_idx + 1: ground_last_idx])
+    ground_sum = np.nansum(wf_per_height[veg_last_idx + 1 : ground_last_idx])
 
     # Calculate vegetation cover assuming veg. to ground ratio = 1
     veg_cover = veg_sum / (veg_sum + ground_sum)
 
     # Calculate cumulative vegetation return
-    veg_cuml = np.nancumsum(wf_per_height[veg_first_idx: veg_last_idx])
+    veg_cuml = np.nancumsum(wf_per_height[veg_first_idx:veg_last_idx])
 
     # Calculate gap probability
     p_gap = 1 - (veg_cuml / (veg_sum + ground_sum))
 
     # Pad gap probability with NaNs to match length of wf_per_height
-    p_gap = np.pad(p_gap,
-                   (veg_first_idx, len(wf_per_height) - veg_last_idx),
-                   constant_values=np.nan)
+    p_gap = np.pad(
+        p_gap,
+        (veg_first_idx, len(wf_per_height) - veg_last_idx),
+        constant_values=np.nan,
+    )
 
     # Foliage accumulation and density
     foliage_accum = -np.log(p_gap) / 0.5
@@ -295,5 +296,5 @@ def calc_gap_prob(wf_per_height: np.ndarray,
         "veg_cover": veg_cover,
         "gap_prob": p_gap,
         "foliage_density": foliage_dens,
-        "foliage_accum": foliage_accum
+        "foliage_accum": foliage_accum,
     }
