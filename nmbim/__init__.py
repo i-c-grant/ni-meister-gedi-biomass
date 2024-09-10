db872d09d20a02d4890b0446a3542a11e5c6a123
Ian Grant <ian.conway.grant@gmail.com>
2024-09-09 20:43:15 -0400

    exposed nmbim class names for easier imports

---
+++ nmbim/__init__.py
@@ -0,0 +1,5 @@
+from .WaveformCollection import WaveformCollection
+from .Beam import Beam
+from .Waveform import Waveform
+from .WaveformProcessor import WaveformProcessor
+from .WaveformWriter import WaveformWriter
