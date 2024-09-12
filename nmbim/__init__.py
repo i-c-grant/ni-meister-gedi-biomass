308e90e911bea42fe745e20ad89358e249fa2887
Ian Grant <ian.conway.grant@gmail.com>
2024-09-12 11:25:32 -0400

    updated nmbim init

--- nmbim/__init__.py
+++ nmbim/__init__.py
@@ -3,3 +3,4 @@ from .Beam import Beam
 from .Waveform import Waveform
 from .WaveformProcessor import WaveformProcessor
 from .WaveformWriter import WaveformWriter
+from .WaveformPlot import WaveformPlot
