ed8feec2030dcbb1e9ec5615f026342de9922d5a
Ian Grant <ian.conway.grant@gmail.com>
2024-09-16 18:24:39 -0400

    refactored with NestedDict and flexible signature

--- nmbim/Waveform.py
+++ nmbim/Waveform.py
@@ -1,254 +1,307 @@
+from copy import deepcopy
+from typing import Any, Dict, Optional, Set, Literal
+
 import h5py
-import numpy as np
 from numpy.typing import ArrayLike
-from typing import Any, Dict, Union, List
+
 from nmbim.Beam import Beam
+from nmbim.NestedDict import NestedDict
+
 
 class Waveform:
-    """Fetches NMBIM-relevant data for one waveform from files or cached data.
-    Responsible for storing raw and processed data, as well as metadata.
-    Not responsible for processing waveform data or opening/closing files.
+    """Stores raw and processed waveform data for one GEDI footprint.
 
-    Attributes
-    ----------
-    metadata: dict[str, Any]
-        Dictionary containing metadata for the waveform.
+    Requires a shot number and either
+    - two Beam objects corresponding to the L1B and L2A beams, or
+    - paths to the raw h5 L1B and L2A files and the name of the beam.
+    Provide cached Beam objects for fast batch processing.
+    Provide file paths and beam names to lazily look up single Waveforms.
 
-    raw: dict[str, Any]
-        Dictionary containing unprocessed waveform data from L1B and L2A files.
-        - wf: np.ndarray
-            The laser return data (from rxwaveform in L1B file).
+    Data is stored in a nested dictionary structure with string paths.
+    Data is stored in four categories: raw, processed, results, and metadata.
 
-        - mean_noise: np.uint16
-            The mean noise value.
-
-        - elev: Dict[str, Union[np.float32, np.float64]]
-            Dictionary containing elevation data.
-
-    processed: dict[str, Any]
-        Dictionary containing processed waveform data. Initially empty.
-
-    results: dict[str, Any]
-        Dictionary containing the results of the NMBIM model.
+    Methods:
+    - get_paths(): Returns a set of terminal paths in the Waveform object.
+    - get_data(path: str): Returns the data stored at the given path.
+    - save_data(data: Any, path: str): Saves data to the given path
     """
 
-    
     def __init__(
-            self,
-            shot_number: int,
-            l1b_beam: Beam,
-            l2a_beam: Beam,
+        self,
+        shot_number: int,
+        l1b_beam: Optional[Beam] = None,
+        l2a_beam: Optional[Beam] = None,
+        l1b_path: Optional[h5py.File] = None,
+        l2a_path: Optional[h5py.File] = None,
+        immutable: bool = True,
     ) -> None:
-        """Initializes the Waveform object.
+        """Initializes the Waveform object. In addition to a shot number,
+        requires either Beam objects or file paths and a beam name.
 
-        Parameters
-        ----------
-        shot_number: int
-            The unique shot number of the waveform.
-
-        l1b_beam: Beam
-            The L1B beam data.
-
-        l2a_beam: Beam
-            The L2A beam data.
+        If immutable is True, data will be deepcopied when retrieved.
         """
+        signature = Waveform._validate_signature(init_args=locals().copy())
 
-        self.l1b_beam: Beam = l1b_beam
-        self.l2a_beam: Beam = l2a_beam
+        # Initialize data dictionary
+        self.immutable: bool = immutable
+        self._data: NestedDict = NestedDict()
 
-        beam_name = l1b_beam.get_beam_name()
-        
-        # Store initial metadata
-        self.metadata = {
-            "shot_number": shot_number,
-            "beam": beam_name,
-        }
+        # Store shot number
+        self.save_data(data=shot_number, path="metadata/shot_number")
 
-        # Get shot index for this waveform (requires initial metadata)
-        # This is the index of the shot within its files,
-        # not the unique shot number
-        self.metadata["shot_index"] = self._get_shot_index()
-        
-        shot_index = self.metadata["shot_index"]
+        if signature == "beam":
+            # Store beams
+            self.l1b_beam = l1b_beam
+            self.l2a_beam = l2a_beam
+            
+            # Store beam name
+            l1b_beam_name = l1b_beam.get_beam_name()
+            l2a_beam_name = l2a_beam.get_beam_name()
+            if l1b_beam_name != l2a_beam_name:
+                raise ValueError(
+                    f"Beam mismatch: L1B beam {l1b_beam_name} "
+                    f"!= L2A beam {l2a_beam_name}"
+                )
+            self.save_data(data=l1b_beam_name, path="metadata/beam")
 
-        # Store geolocation
-        lats: ArrayLike = self.l1b_beam.extract_dataset("geolocation/latitude_bin0")
-        lons: ArrayLike = self.l1b_beam.extract_dataset("geolocation/longitude_bin0")
+            # Get file paths and store
+            self.save_data(data=l1b_beam.get_path(),
+                           path="metadata/l1b_path")
+            self.save_data(data=l2a_beam.get_path(),
+                           path="metadata/l2a_path")
 
-        self.metadata["coords"] = {
-            "lat": lats[shot_index],
-            "lon": lons[shot_index],
-        }
+        if signature == "path":
+            # Store file paths
+            self.save_data(data=l1b_path, path="metadata/l1b_path")
+            self.save_data(data=l2a_path, path="metadata/l2a_path")
 
-        # Get quality flag and number of modes from L2A beam
-        self.metadata["flags"] = {
-            "quality": self.l2a_beam.extract_dataset("quality_flag")[shot_index],
-            "surface": self.l2a_beam.extract_dataset("surface_flag")[shot_index],
-        }
+            # Store beam name
+            beam_name = self._identify_waveform_beam()
+            self.save_data(data=beam_name, path="metadata/beam")
 
-        self.metadata["landcover"] = {
-            "modis_nonvegetated": self.l2a_beam.extract_dataset("land_cover_data/modis_nonvegetated")[shot_index],
-            "modis_treecover": self.l2a_beam.extract_dataset("land_cover_data/modis_treecover")[shot_index],
-            "landsat_treecover": self.l2a_beam.extract_dataset("land_cover_data/landsat_treecover")[shot_index],
-        }
+            # Create beams and store
+            self.l1b_beam = Beam(file=l1b_path, beam=beam_name, cache=False)
+            self.l2a_beam = Beam(file=l2a_path, beam=beam_name, cache=False)
 
-        self.metadata["modes"] = {
-            "num_modes": self.l2a_beam.extract_dataset("num_detectedmodes")[shot_index],
-        }
-
-        # Initialize read-only waveform data (see property below)
-        wf: ArrayLike = self._get_waveform()
-        mean_noise = self.l1b_beam.extract_dataset("noise_mean_corrected")[shot_index]
-        elev: Dict[str, Union[np.float32, np.float64]] = self._get_elev()
-        rh: ArrayLike = self.l2a_beam.extract_dataset("rh")[shot_index]
-
-        self._raw = {
-            "wf": wf,
-            "mean_noise": mean_noise,
-            "elev": elev,
-            "rh": rh,
-        }
-
-        self.processed = {}
-        self.results = {}
-
-    # Make raw data read-only
-    @property
-    def raw(self) -> dict[str, Any]:
-        return self._raw
-
-    @raw.setter
-    def raw(self, value: dict[str, Any]) -> None:
-        raise AttributeError("Raw waveform data cannot be modified.")
-
-    def _get_shot_index(self) -> np.int64:
-        # Find the index of this Waveform's shot within its beam group
-        shot_number: np.int64 = self.metadata["shot_number"]
-        shot_nums_l1b: ArrayLike = self.l1b_beam.extract_dataset("shot_number")
-        shot_nums_l2a: ArrayLike = self.l2a_beam.extract_dataset("shot_number")
-        index_l1b: np.int64 = np.where(shot_nums_l1b == shot_number)[0][0]
-        index_l2a: np.int64 = np.where(shot_nums_l2a == shot_number)[0][0]
-
-        # Ensure shot numbers match between L1B and L2A
-        if index_l1b != index_l2a:
-            raise ValueError(
-                f"File mismatch: L1B shot index {index_l1b} != L2A shot index {index_l2a}"
-            )
+        # Store shot index
+        l1b_index = self.l1b_beam.where_shot(shot_number)
+        l2a_index = self.l2a_beam.where_shot(shot_number)
+        if l1b_index == l2a_index:
+            shot_index = l1b_index
+            self.save_data(data=shot_index, path="metadata/shot_index")
         else:
-            index = index_l1b
+            raise ValueError(
+                f"File mismatch: L1B shot index {l1b_index} != L2A "
+                f"shot index {l2a_index}"
+            )
 
-        return index
-
-    def _get_waveform(self) -> ArrayLike:
-
-        shot_index: np.int64 = self.metadata["shot_index"]
-
-        # Get the array with all waveforms for this beam
-        all_wfs: ArrayLike = self.l1b_beam.extract_dataset("rxwaveform")
-
-        # Get start index of waveform within all_wfs,
-        # converting to 0-based index from GEDI's 1-based index
-        start_idxs: ArrayLike = (
-            self.l1b_beam.extract_dataset("rx_sample_start_index")
-        )
-        wf_start: int = np.int64(start_idxs[shot_index]) - 1
-
-        # Get length of waveform within all_wfs
-        counts: ArrayLike = self.l1b_beam.extract_dataset("rx_sample_count")
-        wf_count = counts[shot_index]
-
-        wf: ArrayLike = all_wfs[wf_start: wf_start + wf_count]
-
-        return wf
-
-    def _get_elev(self) -> Dict[str, Union[np.float32, np.float64]]:
-        shot_index = self.metadata["shot_index"]
-        # Elevation of top of waveform return window
-        top = np.float64(
-            self.l1b_beam.extract_dataset(
-                "geolocation/elevation_bin0"
-            )[shot_index]
-        )
-        
-        # Elevation of bottom of waveform return window
-        bottom = np.float64(
-            self.l1b_beam.extract_dataset(
-                "geolocation/elevation_lastbin"
-            )[shot_index]
+        # Store coordinates
+        lat = self.l1b_beam.extract_value(
+            "geolocation/latitude_bin0", shot_index
         )
 
-        # Elevation of ground, set to elevation of lowest detected waveform mode
-        ground = np.float32(
-            self.l2a_beam.extract_dataset("elev_lowestmode")[shot_index]
+        lon = self.l1b_beam.extract_value(
+            "geolocation/longitude_bin0", shot_index
         )
 
-        elev = {"top": top, "bottom": bottom, "ground": ground}
+        self.save_data(
+            data={"lat": lat, "lon": lon},
+            path="metadata/coords",
+        )
 
-        return elev
+        # Store quality flags
+        qual_flag = self.l2a_beam.extract_value("quality_flag", shot_index)
+        surf_flag = self.l2a_beam.extract_value("surface_flag", shot_index)
+        flag_dict = {"quality": qual_flag, "surface": surf_flag}
+        self.save_data(data=flag_dict, path="metadata/flags")
+
+        # Store number of detected modes
+        self.save_data(
+            data={
+                "num_modes": self.l2a_beam.extract_value(
+                    "num_detectedmodes", shot_index
+                )
+            },
+            path="metadata/modes",
+        )
+
+        # Store land cover data
+        self.save_data(
+            data={
+                "modis_nonvegetated": self.l2a_beam.extract_value(
+                    "land_cover_data/modis_nonvegetated", shot_index
+                ),
+                "modis_treecover": self.l2a_beam.extract_value(
+                    "land_cover_data/modis_treecover", shot_index
+                ),
+                "landsat_treecover": self.l2a_beam.extract_value(
+                    "land_cover_data/landsat_treecover", shot_index
+                ),
+            },
+            path="metadata/landcover",
+        )
+
+        # Store raw waveform data from L1B beam by subsetting the waveform
+        # (subtract 1 from start index to convert to 0-based indexing)
+        wf_start: int = (
+            l1b_beam.extract_value("rx_sample_start_index", shot_index) - 1
+        )
+        wf_len: int = l1b_beam.extract_value("rx_sample_count", shot_index)
+        wf: ArrayLike = l1b_beam.extract_dataset("rxwaveform")[
+            wf_start : wf_start + wf_len
+        ]
+        self.save_data(data=wf, path="raw/wf")
+
+        # Store mean noise value
+        self.save_data(
+            data=l1b_beam.extract_value("noise_mean_corrected", shot_index),
+            path="raw/mean_noise",
+        )
+
+        # Store relative height
+        self.save_data(
+            data=l2a_beam.extract_value("rh", shot_index), path="raw/rh"
+        )
+
+        # Store elevation
+        self.save_data(
+            data={
+                "top": self.l1b_beam.extract_value(
+                    "geolocation/elevation_bin0", shot_index
+                ),
+                "bottom": self.l1b_beam.extract_value(
+                    "geolocation/elevation_lastbin", shot_index
+                ),
+                "ground": self.l2a_beam.extract_value(
+                    "elev_lowestmode", shot_index
+                ),
+            },
+            path="raw/elev",
+        )
+
+    @staticmethod
+    def _validate_signature(init_args: Dict[str, Any]) -> Literal["beam", "path"]:
+        # Verify that the initialization signature is valid
+        # (either two Beam objects or two file paths)
+        # and return the signature type
+
+        # Define helper functions to check for None values
+        def any_none(*args: Any) -> bool:
+            return any(arg is None for arg in args)
+
+        def all_none(*args: Any) -> bool:
+            return all(arg is None for arg in args)
+
+        # Two options are valid:
+        # 1) two Beam objects and no file paths or beam name
+        if not any_none(
+            init_args["l1b_beam"], init_args["l2a_beam"]
+        ) and all_none(
+            init_args["l1b_path"], init_args["l2a_path"],
+        ):
+            signature = "beam"
+        # or 2) two file paths and a beam name but no Beam objects
+        elif all_none(
+            init_args["l1b_beam"], init_args["l2a_beam"]
+        ) and not any_none(
+            init_args["l1b_path"], init_args["l2a_path"],
+        ):
+            signature = "path"
+        else:
+            raise ValueError(
+                "Invalid Waveform initialization signature. "
+                "Provide either 1) two Beam objects or 2) two file paths, "
+                "but not a mix of both."
+            )
+        return signature
+
+    def _identify_waveform_beam(self) -> Optional[str]:
+        """Determine which beam the waveform belongs to"""
+        l1b_path = self.get_data("metadata/l1b_path")
+        l2a_path = self.get_data("metadata/l2a_path")
+        shot_number = self.get_data("metadata/shot_number")
+
+        with h5py.File(l1b_path) as l1b_file:
+            l1b_beam: str = Waveform._which_beam(
+                self.get_data("metadata/shot_number"), l1b_file)
+
+        with h5py.File(l2a_path) as l2a_file:
+            l2a_beam: str = Waveform._which_beam(
+                self.get_data("metadata/shot_number"), l2a_file)
+
+        if l1b_beam is None or l2a_beam is None:
+            raise ValueError("Unable to identify beam for L1B or L2A file.")
+
+        if l1b_beam != l2a_beam:
+            raise ValueError(
+                f"Beam mismatch: L1B beam {l1b_beam} != L2A beam {l2a_beam}"
+            )
+
+        return l1b_beam
+
+    @staticmethod
+    def _which_beam(shot_number: int, file: h5py.File) -> Optional[str]:
+        """Determine which beam a waveform belongs to"""
+        for key in file.keys():
+            if key != "METADATA":
+                beam = key
+                shot_numbers = file[beam]["shot_number"]
+                if shot_number in shot_numbers:
+                      return beam
+        return None
+
+    def get_paths(self) -> Set[str]:
+        """Returns a set of terminal paths in the Waveform object."""
+        return self._data.get_paths()
 
     def get_data(self, path: str) -> Any:
-        """
-        Get data from the Waveform object.
-
-        Parameters
-        ----------
-        path: str
-            The path to the data in the Waveform object, e.g. "raw/wf".
-
-        Returns
-        -------
-        data: Any
-            The data from the waveform.
-        """
-        keys = path.split("/")
-
-        if not hasattr(self, keys[0]):
-            raise AttributeError(
-                f"'{self.__class__.__name__}' object has no attribute '{keys[0]}'"
-            )
-
-        # Get top-level dictionary
-        data = getattr(self, keys[0])
-
-        # Traverse dictionary to get data
-        for key in keys[1:]:
-            data = data[key] # type: ignore
-
+        """Returns the data stored at the given path."""
+        data = self._data.get_data(path)
+        if self.immutable:
+            data = deepcopy(data)
         return data
 
     def save_data(self, data: Any, path: str) -> None:
-        """Save data to the Waveform.
-
-        Parameters
-        ----------
-        data: Any
-            The data to save.
-
-        path: str
-            Path to the data in the Waveform object, e.g. "processed/heights".
-        """
-
-        keys = path.split("/")
-        if not hasattr(self, keys[0]):
-            raise AttributeError(
-                f"'{self.__class__.__name__}' object has no attribute '{keys[0]}'"
+        """Saves data to the given path in the Waveform object. If a
+        dictionary is passed, its keys will be saved as sub-entries
+        under the specified path."""
+        valid_first_keys = ["raw", "processed", "results", "metadata"]
+        if path.split("/")[0] not in valid_first_keys:
+            raise ValueError(
+                "Invalid path provided. Must start with 'raw',"
+                "'processed', 'results', or 'metadata'"
             )
 
-        data_dict = getattr(self, keys[0])
-        for key in keys[1:-1]:
-            # Create nested dicts to the save path if they don't exist
-            if key not in data_dict:
-                data_dict[key] = {}
-            data_dict = data_dict[key]
-        data_dict[keys[-1]] = data
+        self._data.save_data(data, path, overwrite=False)
 
     def __eq__(self, other: object) -> bool:
         if not isinstance(other, Waveform):
             return False
-        else:
-            return self.metadata == other.metadata
+
+        my_shot_number = self.get_data("metadata/shot_number")
+        other_shot_number = other.get_data("metadata/shot_number")
+        my_beam = self.get_data("metadata/beam")
+        other_beam = other.get_data("metadata/beam")
+
+        is_equal = (
+            my_shot_number == other_shot_number and my_beam == other_beam
+        )
+
+        return is_equal
 
     def __hash__(self) -> int:
-        beam = self.metadata["beam"]
-        shot_number = self.metadata["shot_number"]
+        beam = self.get_data("metadata/beam")
+        shot_number = self.get_data("metadata/shot_number")
         return hash((beam, shot_number))
+
+    def __str__(self) -> str:
+        return f"Waveform {self.get_data('metadata/shot_number')}"
+
+    def __repr__(self) -> str:
+        rep = (
+            f"Waveform(shot_number={self.get_data('metadata/shot_number')}, "
+            f"Beam: {self.get_data('metadata/beam')}, "
+            f"Coords: {self.get_data('metadata/coords')}, "
+            f"Paths: {sorted(self.get_paths())}"
+        )
+        return rep
