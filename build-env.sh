8f659ecefa0d515070c58a02f996b26c584fc7ac
Ian Grant <ian.conway.grant@gmail.com>
2024-09-19 18:24:05 -0400

    formatting fixes

--- build-env.sh
+++ build-env.sh
@@ -3,4 +3,4 @@
 # source activate base
 basedir=$( cd "$(dirname "$0")" ; pwd -P)
 echo "Creating conda environment from ${basedir}/environment.yml"
-conda env create --name nmbim-env -f ${basedir}/environment.yml
+conda env create --name "nmbim-env" -f ${basedir}/environment.yml
