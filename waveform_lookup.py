4d268418462e2953edadf0b495dafa3416793d1c
Ian Grant <ian.conway.grant@gmail.com>
2024-09-19 18:26:16 -0400

    updated waveform lookup app with plotting and saving

--- waveform_lookup.py
+++ waveform_lookup.py
@@ -1,14 +1,21 @@
 import click
 from nmbim import (
-    WaveformCollection, WaveformPlot, processing_pipelines, app_utils
+    processing_pipelines,
+    app_utils,
+    Waveform,
+    WaveformProcessor,
+    WaveformPlotter,
 )
 
+import h5py
+
 @click.command()
-@click.argument('l1b', type=click.Path(exists=True))
-@click.argument('l2a', type=click.Path(exists=True))
-def cli(l1b, l2a):
+@click.argument("l1b_path", type=click.Path(exists=True))
+@click.argument("l2a_path", type=click.Path(exists=True))
+def cli(l1b_path, l2a_path):
     """
-    Load L1B and L2A files, process the WaveformCollection, and allow the user to query shot numbers to plot.
+    Load L1B and L2A files, process the WaveformCollection, and allow
+    the user to query shot numbers to plot.
     """
     # Processing pipeline
     processor_params = processing_pipelines.pipeline_test_veg_ground_sep
@@ -16,37 +23,65 @@ def cli(l1b, l2a):
     # Initialize filters (from app_utils)
     my_filters = app_utils.define_filters()
 
-    # Load the WaveformCollection with caching (but not beamwise)
-    click.echo("Loading waveforms...")
-    waveforms = WaveformCollection(input_l1b=l1b,
-                                   input_l2a=l2a,
-                                   cache_beams=True,
-                                   filters=my_filters)
-    click.echo(f"Loaded {len(waveforms)} waveforms.")
+    # Define the layers for plotting
+    layers = {
+        "Smoothed waveform": {"x": "processed/dp_dz",
+                                      "color": "black",
+                                      "type": "line"},
+        "Estimated ground return": {"x": "processed/ground_return",
+                                    "color": "blue",
+                                    "type": "line"},
+        "Estimated veg. return": {"x": "processed/dp_dz_veg_only",
+                                  "color": "red",
+                                  "type": "line"},
+        "Raw returns": {"x": "processed/wf_raw_scaled",
+                        "color": "gray",
+                        "type": "scatter"},
+    }
 
-    # Process the waveforms
-    click.echo("Processing waveforms...")
-    app_utils.process_waveforms(waveforms, processor_params)
-    click.echo("Waveforms processed.")
+    plotter = WaveformPlotter(layer_dict=layers,
+                              y_lab="Height (m)",
+                              x_lab="Waveform returns")
 
+    # y path is constant for all layers
+    for layer in layers:
+        layers[layer]["y"] = "processed/ht"
+
+    with h5py.File(l1b_path, "r") as l1b, h5py.File(l2a_path, "r") as l2a:
     # CLI loop to query shot numbers and plot corresponding waveforms
-    while True:
-        shot_num = click.prompt('Enter shot number to plot (or type "quit" to exit)', type=str)
-        
-        if shot_num.lower() == "quit":
-            click.echo("Exiting the application.")
-            break
+        while True:
+            input = click.prompt(
+                ("Enter shot number to plot, 'quit' to exit, or 'save' "
+                 "to save last plot)"), 
+                type=str)
 
-        try:
-            # Assuming shot_num is a valid index or can be converted to an integer
-            shot_idx = int(shot_num)
-            waveform = waveforms.get_waveform(shot_idx)
+            if input.lower() == "quit":
+                click.echo("Exiting the application.")
+                break
 
-            # Plot the waveform
-            WaveformPlot(waveform).segmentation_plot()
+            elif input.lower() == "save":
+                plotter.save()
+                click.echo("Plot saved.")
 
-        except (ValueError, IndexError):
-            click.echo(f"Invalid shot number: {shot_num}. Please try again.")
+            else:
+                shot_num = input
+                try:
+                    shot_number = int(shot_num)
+                    waveform = Waveform(
+                        l1b=l1b,
+                        l2a=l2a,
+                        shot_number=shot_number,
+                    )
+
+                    app_utils.process_waveforms(waveform, processor_params)
+
+                    # Plot the waveform
+                    plotter.plot(waveform)
+
+                except (ValueError, IndexError):
+                    click.echo(f"Invalid shot number: {shot_num}. "
+                               f"Please try again.")
+
 
 if __name__ == "__main__":
     cli()
