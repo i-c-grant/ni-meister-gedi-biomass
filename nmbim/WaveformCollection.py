687f56c9a82a6bf365455bfdc5020fcad2c25742
Ian Grant <ian.conway.grant@gmail.com>
2024-09-19 23:10:36 -0400

    changed WaveformCollection to take h5py.File objects, not paths

--- nmbim/WaveformCollection.py
+++ nmbim/WaveformCollection.py
@@ -36,8 +36,8 @@ class WaveformCollection:
 
     def __init__(
         self,
-        input_l1b: str,
-        input_l2a: str,
+        l1b: h5py.File,
+        l2a: h5py.File,
         filters: Union[Filter, List[Filter]] = None,
         cache_beams: bool = True,
         beams=None,
@@ -46,8 +46,8 @@ class WaveformCollection:
         Initialize the WaveformCollection by loading waveform data from two HDF5 files.
 
         Parameters:
-            input_l1b (str): Path to the L1B HDF5 file.
-            input_l2a (str): Path to the L2A HDF5 file.
+            l1b (h5py.File): L1B HDF5 file object.
+            l2a (h5py.File): L2A HDF5 file object.
             filters (Union[Filter, List[Filter]], optional): A filter function or list
                 of functions to apply to each waveform. Defaults to None (no filters).
             cache_beams (bool, optional): Whether to cache beam data in memory.
@@ -55,8 +55,8 @@ class WaveformCollection:
             beams (List[str], optional): List of beam names to process.
         """
 
-        self.l1b_path = Path(input_l1b)
-        self.l2a_path = Path(input_l2a)
+        self.l1b_path = l1b.filename
+        self.l2a_path = l2a.filename
 
         self.waveforms = []
         self.cache_beams = cache_beams
@@ -69,56 +69,52 @@ class WaveformCollection:
 
         self.filters = filters
 
-        # Read in waveform data
-        with h5py.File(input_l1b, "r") as l1b, h5py.File(
-            input_l2a, "r"
-        ) as l2a:
-            # If no beams are specified, process all beams
-            if beams is None:
-                beams: List[str] = [
-                    key for key in l1b.keys() if key != "METADATA"
-                ]
+        # If no beams are specified, process all beams
+        if beams is None:
+            beams: List[str] = [
+                key for key in l1b.keys() if key != "METADATA"
+            ]
 
-            # Construct waveforms for each beam, caching a beam at a time
-            for beam_name in beams:
-                l1b_beam = Beam(
-                    file=l1b, beam=beam_name, cache=self.cache_beams
+        # Construct waveforms for each beam, caching a beam at a time
+        for beam_name in beams:
+            l1b_beam = Beam(
+                file=l1b, beam=beam_name, cache=self.cache_beams
+            )
+            l2a_beam = Beam(
+                file=l2a, beam=beam_name, cache=self.cache_beams
+            )
+
+            shot_numbers_l1b: ArrayLike = l1b_beam.extract_dataset(
+                "shot_number"
+            )
+            shot_numbers_l2a: ArrayLike = l2a_beam.extract_dataset(
+                "shot_number"
+            )
+
+            # Check that shot numbers match between files
+            if not np.array_equal(shot_numbers_l1b, shot_numbers_l2a):
+                raise ValueError(
+                    f"Shot numbers don't match between {input_l1b}"
+                    f"and {input_l2a}"
                 )
-                l2a_beam = Beam(
-                    file=l2a, beam=beam_name, cache=self.cache_beams
+
+            # Check that both files have the same number of shots
+            if len(shot_numbers_l1b) != len(shot_numbers_l2a):
+                raise ValueError(
+                    f"{input_l1b} has {len(shot_numbers_l1b)} shots,"
+                    f"but {input_l2a} has {len(shot_numbers_l2a)} shots."
                 )
 
-                shot_numbers_l1b: ArrayLike = l1b_beam.extract_dataset(
-                    "shot_number"
-                )
-                shot_numbers_l2a: ArrayLike = l2a_beam.extract_dataset(
-                    "shot_number"
-                )
+            shot_numbers = shot_numbers_l1b
 
-                # Check that shot numbers match between files
-                if not np.array_equal(shot_numbers_l1b, shot_numbers_l2a):
-                    raise ValueError(
-                        f"Shot numbers don't match between {input_l1b}"
-                        f"and {input_l2a}"
-                    )
+            # Create the Waveforms for this beam
+            waveform_args = {"l1b_beam": l1b_beam, "l2a_beam": l2a_beam}
 
-                # Check that both files have the same number of shots
-                if len(shot_numbers_l1b) != len(shot_numbers_l2a):
-                    raise ValueError(
-                        f"{input_l1b} has {len(shot_numbers_l1b)} shots,"
-                        f"but {input_l2a} has {len(shot_numbers_l2a)} shots."
-                    )
-
-                shot_numbers = shot_numbers_l1b
-
-                # Create the Waveforms for this beam
-                waveform_args = {"l1b_beam": l1b_beam, "l2a_beam": l2a_beam}
-
-                for shot_number in shot_numbers:
-                    waveform_args["shot_number"] = shot_number
-                    new_wf = Waveform(**waveform_args)
-                    if self.filter_waveform(new_wf):
-                        self.add_waveform(new_wf)
+            for shot_number in shot_numbers:
+                waveform_args["shot_number"] = shot_number
+                new_wf = Waveform(**waveform_args)
+                if self.filter_waveform(new_wf):
+                    self.add_waveform(new_wf)
 
             if len(self) == 0:
                 warnings.warn(
