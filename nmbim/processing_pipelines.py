e2cf3c8791b89bdf940a79cde6ec3cac475108a2
Ian Grant <ian.conway.grant@gmail.com>
2024-09-21 17:07:24 -0400

    reorganized pipeline into pre- and post-normalization phases

--- nmbim/processing_pipelines.py
+++ nmbim/processing_pipelines.py
@@ -1,6 +1,8 @@
 import nmbim.algorithms as algorithms
 
 biwf_pipeline = {
+    ### Pre-normalization processing ###
+    # Calculate height array and dz value
     "height": {
         "alg_fun": algorithms.calc_height,
         "input_map": {
@@ -12,92 +14,112 @@ biwf_pipeline = {
         "output_path": "processed/ht",
         "params": {},
     },
-    "dz": {
+    "calc_dz": {
         "alg_fun": algorithms.calc_dz,
         "input_map": {"ht": "processed/ht"},
         "output_path": "processed/dz",
         "params": {},
     },
-    "noise": {
+    # Remove initial signal noise from the waveform
+    "remove_initial_noise": {
         "alg_fun": algorithms.remove_noise,
         "input_map": {"wf": "raw/wf", "mean_noise": "raw/mean_noise"},
         "output_path": "processed/wf_noise_removed",
         "params": {},
     },
-    "normalize": {
-        "alg_fun": algorithms.normalize_waveform,
+    # Smooth the waveform
+    "smooth": {
+        "alg_fun": algorithms.smooth_waveform,
         "input_map": {"wf": "processed/wf_noise_removed"},
-        "output_path": "processed/wf_noise_norm",
+        "output_path": "processed/wf_noise_removed_smooth",
+        "params": {"sd": 8},
+    },
+    # Identify borders of vegetation and ground returns
+    "segment": {
+        "alg_fun": algorithms.separate_veg_ground,
+        "input_map": {
+            "wf": "processed/wf_noise_removed_smooth",
+            "ht": "processed/ht",
+            "dz": "processed/dz",
+            "rh": "raw/rh",
+        },
+        "params": {"veg_floor": 5,
+                   "veg_buffer": 5,},
+        "output_path": "processed/veg_ground_sep",
+    },
+    # Calculate and remove residual noise from smoothed waveform
+    "calc_resid_noise": {
+        "alg_fun": algorithms.calc_noise,
+        "input_map": {
+            "wf": "processed/wf_noise_removed_smooth",
+            "veg_top": "processed/veg_ground_sep/veg_top",
+            "ground_bottom": "processed/veg_ground_sep/ground_bottom",
+            "ht": "processed/ht",
+        },
+        "output_path": "processed/residual_noise",
+        "params": {"noise_ratio": 2},
+    },
+    "remove_resid_noise": {
+        "alg_fun": algorithms.remove_noise,
+        "input_map": {
+            "wf": "processed/wf_noise_removed_smooth",
+            "mean_noise": "processed/residual_noise",
+        },
+        "output_path": "processed/wf_all_noise_removed",
         "params": {},
     },
-    # Scale the normalized, non-smoothed waveform for visualization
-    # using dp_dz. This is just to be able to compare the raw waveform
-    # to the smoothed waveform; this is not part of the actual processing.
-    "scale_raw": {
-        "alg_fun": algorithms.calc_dp_dz,
+    # Remove residual noise from raw waveform too for visualization
+    "remove_resid_noise_raw": {
+        "alg_fun": algorithms.remove_noise,
         "input_map": {
-            "wf": "processed/wf_noise_norm",
-            "dz": "processed/dz",
+            "wf": "processed/wf_noise_removed",
+            "mean_noise": "processed/residual_noise",
         },
+        "output_path": "processed/wf_noise_removed_raw",
+        "params": {},
+    },
+    # Scale the raw waveform for visualization
+    "scale_raw": {
+        "alg_fun": algorithms.scale_raw_wf,
+        "input_map": {"wf_raw": "processed/wf_noise_removed_raw",
+                      "wf_smooth": "processed/wf_all_noise_removed",
+                      "dz": "processed/dz"},
         "output_path": "processed/wf_raw_scaled",
         "params": {},
     },
-    "smooth": {
-        "alg_fun": algorithms.smooth_waveform,
-        "input_map": {"wf": "processed/wf_noise_norm"},
-        "output_path": "processed/wf_noise_norm_smooth",
-        "params": {"sd": 5},
+ 
+    ### Post-normalization processing ###
+    # Normalize the smoothed, noise-removed waveform
+    "normalize": {
+        "alg_fun": algorithms.normalize_waveform,
+        "input_map": {"wf": "processed/wf_all_noise_removed"},
+        "output_path": "processed/wf_norm",
+        "params": {},
     },
+    # Divide the normalized waveform by the dz value
     "dp_dz": {
         "alg_fun": algorithms.calc_dp_dz,
         "input_map": {
-            "wf": "processed/wf_noise_norm_smooth",
+            "wf": "processed/wf_norm",
             "dz": "processed/dz",
         },
         "output_path": "processed/dp_dz",
         "params": {},
     },
-    "segment": {
-        "alg_fun": algorithms.separate_veg_ground,
-        "input_map": {
-            "wf": "processed/dp_dz",
-            "ht": "processed/ht",
-            "dz": "processed/dz",
-            "rh": "raw/rh",
-        },
-        "params": {"veg_floor": 5},
-        "output_path": "processed/veg_ground_sep",
-    },
-    # Calculate the residual noise using the segmented waveform
-    "calc_resid_noise": {
-        "alg_fun": algorithms.calc_noise,
-        "input_map": {
-            "wf": "processed/dp_dz",
-            "veg_top": "processed/veg_ground_sep/veg_top",
-            "ground_bottom": "processed/veg_ground_sep/ground_bottom",
-        },
-        "output_path": "processed/residual_noise",
-        "params": {},
-    },
-    "remove_resid_noise": {
-        algorithm: algorithms.remove_noise,
-        "input_map": {
-            "wf": "processed/dp_dz",
-            "mean_noise": "processed/residual_noise",
-        },
-        "output_path": "processed/dp_dz_noise_removed",
-        params: {},
-    },
+    # Calculate the ground return
     "ground_return": {
         "alg_fun": algorithms.create_ground_return,
         "input_map": {
-            "wf": "processed/dp_dz_noise_removed",
+            "wf": "processed/dp_dz",
             "ht": "processed/ht",
-            "ground_return_max_height": "processed/veg_ground_sep/ground_bottom",
+            "ground_return_max_height": ("processed/"
+                                         "veg_ground_sep/"
+                                         "ground_bottom"),
         },
         "output_path": "processed/ground_return",
         "params": {"sd_ratio": 0.25},
     },
+    # Isolate the vegetation return by removing the ground return
     "isolate_veg": {
         "alg_fun": algorithms.isolate_vegetation,
         "input_map": {
@@ -109,12 +131,14 @@ biwf_pipeline = {
         "output_path": "processed/dp_dz_veg_only",
         "params": {},
     },
+    # Calculate the biomass index using the isolated vegetation return
     "calc_biwf": {
         "alg_fun": algorithms.calc_biomass_index,
         "input_map": {
             "dp_dz": "processed/dp_dz_veg_only",
             "dz": "processed/dz",
             "ht": "processed/ht",
+            "n_modes": "metadata/modes/num_modes",
         },
         "output_path": "results/biomass_index",
         "params": {"hse": 1.7},
