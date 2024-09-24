a312ae65a2ee372f79125f8f28039c59271dfb24
Ian Grant <ian.conway.grant@gmail.com>
2024-09-24 15:18:07 -0400

    added function to convert GEDI filenames to output name

--- nmbim/app_utils.py
+++ nmbim/app_utils.py
@@ -10,6 +10,27 @@ from nmbim import (
     WaveformWriter,
 )
 
+def build_output_filename(l1b_path: str, l2a_path: str) -> str:
+    """Build output filename from input filenames"""
+
+    # For each of l1b and l2a, extract the unique, common part of filename:
+    # datetime of aquisition + orbit number + granule number within orbit
+
+    # See <https://lpdaac.usgs.gov/data/get-started-data/collection-overview/
+    # missions/gedi-overview/> for filename structure
+
+    l1b_base = l1b_path.split("/")[-1].split(".")[0].split("_")
+    l1b_base = "_".join(l1b_base[2:5])
+    
+    l2a_base = l2a_path.split("/")[-1].split(".")[0].split("_")
+    l2a_base = "_".join(l2a_base[2:5])
+
+    if l1b_base == l2a_base:
+        return l1b_base
+    else:
+        raise ValueError("Input filenames do not match: {l1b_base}, {l2a_base}")
+    
+
 def get_beam_names():
     """Return a list of beam names"""
     return [
