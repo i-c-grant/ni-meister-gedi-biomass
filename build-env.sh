eb3cf26a8a0aadd9a9872c00abaaefb85ddb1ebc
Ian Grant <ian.conway.grant@gmail.com>
2024-09-24 12:38:26 -0400

    updated build script

--- build-env.sh
+++ build-env.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env -S bash --login
+set -euo pipefail
 
-source activate base
 basedir=$( cd "$(dirname "$0")" ; pwd -P)
 echo "Creating conda environment from ${basedir}/environment.yml"
 conda env create --name "nmbim-env" -f ${basedir}/environment.yml
