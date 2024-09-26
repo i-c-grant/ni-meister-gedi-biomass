d050c68cf61cf30806fb53fdd52ceb9871a1b6c3
Ian Grant <ian.conway.grant@gmail.com>
2024-09-26 15:49:13 -0400

    fixed function name typo in download_gedi_granules.py

--- download_gedi_granules.py
+++ download_gedi_granules.py
@@ -10,8 +10,8 @@ from access_gedi import maap_utils
 @click.argument('l2a_ur', type=str)
 @click.argument('output_dir', type=str)
 def main(l1b_ur, l2a_ur, output_dir):
-    l1b_collection: str = maap_utils.get_collection("l1b")
-    l2a_collection: str = maap_utils.get_collection("l2a")
+    l1b_collection: str = maap_utils.get_collection_id("l1b")
+    l2a_collection: str = maap_utils.get_collection_id("l2a")
 
     l1b_granule: Granule = maap_utils.find_unique_granule(l1b_ur,
                                                           l1b_collection)
