ecc4006298a1dc015347b01beca960c628f760d4
Ian Grant <ian.conway.grant@gmail.com>
2025-05-07 00:12:34 -0400

    style: break long condition into multiple lines

--- maap_utils/JobManager.py
+++ maap_utils/JobManager.py
@@ -128,7 +128,8 @@ class JobManager:
         cycle_count = 0
         try:
             with tqdm(total=len(self.jobs), desc="Job Status") as pbar:
-                while not all(state in self.FINAL_STATES for state in self.job_states.values()):
+                while not all(state in self.FINAL_STATES
+                              for state in self.job_states.values()):
                     updated = self._update_states(batch_size=INNER_BATCH,
                                                   delay=0)
                     self.progress += updated
